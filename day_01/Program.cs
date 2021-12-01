075877d45fb8eaba8ebedcb053cbe912bffc30b4
tkniki <trankimniki@gmail.com>
2021-12-01 09:19:01 +0100

    Program.cs

---
+++ day_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace day_01
+{
+    class Program
+    {
+        static Boolean DoesItIncrease (int[] arr, int index)
+        {
+            if (index != 0 && arr[index] > arr[index-1])
+            {
+               return true;
+            }
+            return false;
+        }
+
+        static int CountIncreased (int[] arr)
+        {
+            int num = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (DoesItIncrease(arr, i))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] measurements = new int[2000];
+            StreamReader sr = new StreamReader("C:\\Users\\trank\\Documents\\Advent of Code 2021\\01_input.txt");
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                measurements[i] = Convert.ToInt32(sr.ReadLine());
+                /*Console.WriteLine(measurements[i]);*/
+            }
+
+
+
+            Console.WriteLine(CountIncreased(measurements));
+        }
+    }
+}
